8fe6fd7c604f3c2eb2f9c240e9f554f9859c49e9
BelliaNino <bellianino1996@gmail.com>
2026-02-17 19:17:31 +0100

    add ex n°2

---
+++ main.js
@@ -0,0 +1,144 @@
+console.log('ok');
+
+
+
+
+
+
+
+
+
+/*Snack2**
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+
+//creare un array con all'interno degli objects 
+const ucl = [
+    {
+        club: 'Inter',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Milan',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Juventus',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Arsenal',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Chelsea',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Manchester City',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Liverpool',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Tottenham',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Real Madrid',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Atletico Madrid',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Barcelona',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Bayern Munich',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Borussia Dortmund',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'PSG',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Fluminense',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Flamengo',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'River Plate',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Boca Juniors',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Colo Colo',
+        pt: 0,
+        fc: 0
+    },
+    {
+        club: 'Ajax',
+        pt: 0,
+        fc: 0
+    },
+];
+
+
+//formulare dei numeri casuali per i punti e per i falli commessi 
+for (let i = 0; i < ucl.length; i++) {
+    ucl[i].pt = Math.floor(Math.random() * 90) + 1;
+    ucl[i].fc = Math.floor(Math.random() * 50) + 1;
+    console.log(ucl[i])
+}
+
+//creare un array vuota per inserire la squadra e i falli commessi 
+let teamFaul = []
+
+//grazie al ciclo for inserire nella nuova array solo i club con i rispettivi falli commessi
+for (let i = 0; i < ucl.length; i++) {
+    teamFaul.push({
+        club: ucl[i].club,
+        falli: ucl[i].fc
+    })
+
+}
+
+//stampo l'array in console
+console.log(teamFaul);
